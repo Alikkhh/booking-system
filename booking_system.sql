b544cab1c7a505af910ee293ee02dd96b6f2ce99
Alikhan <78583242+Alikkhh@users.noreply.github.com>
2022-05-23 19:46:06 +0600

    Add files via upload

---
+++ booking_system.sql
@@ -0,0 +1,313 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Июн 03 2021 г., 06:58
+-- Версия сервера: 10.4.19-MariaDB
+-- Версия PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `booking_system`
+--
+
+DELIMITER $$
+--
+-- Процедуры
+--
+CREATE DEFINER=`Harry`@`localhost` PROCEDURE `GetFlightStatistics` (IN `j_date` DATE)  BEGIN
+ select flight_no,departure_date,IFNULL(no_of_passengers, 0) as no_of_passengers,total_capacity from (
+select f.flight_no,f.departure_date,sum(t.no_of_passengers) as no_of_passengers,j.total_capacity 
+from flight_details f left join ticket_details t 
+on t.booking_status='CONFIRMED' 
+and t.flight_no=f.flight_no 
+and f.departure_date=t.journey_date 
+inner join jet_details j on j.jet_id=f.jet_id
+group by flight_no,journey_date) k where departure_date=j_date;
+ END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` varchar(20) NOT NULL,
+  `pwd` varchar(30) DEFAULT NULL,
+  `staff_id` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(35) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Дамп данных таблицы `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `pwd`, `staff_id`, `name`, `email`) VALUES
+('Alikh', 'qwerty123', '101', 'Alikhan Kuandykov', 'alihankuandikov@gmal.com');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` varchar(20) NOT NULL,
+  `pwd` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(35) DEFAULT NULL,
+  `phone_no` varchar(15) DEFAULT NULL,
+  `address` varchar(35) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Дамп данных таблицы `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `pwd`, `name`, `email`, `phone_no`, `address`) VALUES
+('Alikh', 'barcatop', 'Alikhan', 'alihankuandikov@gmail.com', '87473230710', 'Akan Seri 21');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `frequent_psng_details`
+--
+
+CREATE TABLE `frequent_psng_details` (
+  `frequent_psng_no` varchar(20) NOT NULL,
+  `customer_id` varchar(20) DEFAULT NULL,
+  `mileage` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `passengers`
+--
+
+CREATE TABLE `passengers` (
+  `passenger_id` int(10) NOT NULL,
+  `pnr` varchar(15) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `age` int(3) DEFAULT NULL,
+  `gender` varchar(8) DEFAULT NULL,
+  `meal_choice` varchar(5) DEFAULT NULL,
+  `frequent_psng_no` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `payment_details`
+--
+
+CREATE TABLE `payment_details` (
+  `payment_id` varchar(20) NOT NULL,
+  `pnr` varchar(15) DEFAULT NULL,
+  `payment_date` date DEFAULT NULL,
+  `payment_amount` int(6) DEFAULT NULL,
+  `payment_mode` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Триггеры `payment_details`
+--
+DELIMITER $$
+CREATE TRIGGER `update_ticket_after_payment` AFTER INSERT ON `payment_details` FOR EACH ROW UPDATE ticket_details
+     SET booking_status='CONFIRMED', payment_id= NEW.payment_id
+   WHERE pnr = NEW.pnr
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `railway_details`
+--
+
+CREATE TABLE `railway_details` (
+  `railway_no` varchar(10) NOT NULL,
+  `from_city` varchar(20) DEFAULT NULL,
+  `to_city` varchar(20) DEFAULT NULL,
+  `departure_date` date NOT NULL,
+  `arrival_date` date DEFAULT NULL,
+  `departure_time` time DEFAULT NULL,
+  `arrival_time` time DEFAULT NULL,
+  `seats_economy` int(5) DEFAULT NULL,
+  `seats_business` int(5) DEFAULT NULL,
+  `price_economy` int(10) DEFAULT NULL,
+  `price_business` int(10) DEFAULT NULL,
+  `train_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Дамп данных таблицы `railway_details`
+--
+
+INSERT INTO `railway_details` (`railway_no`, `from_city`, `to_city`, `departure_date`, `arrival_date`, `departure_time`, `arrival_time`, `seats_economy`, `seats_business`, `price_economy`, `price_business`, `train_id`) VALUES
+('AA101', 'Almaty', 'Pavlodar', '2021-07-01', '2021-07-03', '21:00:00', '01:00:00', 130, 70, 5000, 7500, '10001'),
+('AA102', 'Semey', 'Aktau', '2021-06-09', '2021-06-13', '10:00:00', '12:00:00', 180, 95, 2500, 3000, '10002'),
+('AA103', 'Nur-Sultan', 'Shymkent', '2021-07-03', '2017-07-05', '17:00:00', '17:45:00', 150, 75, 1200, 1500, '10004'),
+('AA104', 'Nur-Sultan', 'Almaty', '2021-06-12', '2021-06-13', '09:00:00', '09:17:00', 30, 20, 500, 750, '10003'),
+('AA106', 'Uralsk', 'Nur-Sultan', '2021-06-17', '2017-06-20', '13:00:00', '14:00:00', 150, 100, 3000, 3750, '10007');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `ticket_details`
+--
+
+CREATE TABLE `ticket_details` (
+  `pnr` varchar(15) NOT NULL,
+  `date_of_reservation` date DEFAULT NULL,
+  `railway_no` varchar(10) DEFAULT NULL,
+  `journey_date` date DEFAULT NULL,
+  `class` varchar(10) DEFAULT NULL,
+  `booking_status` varchar(20) DEFAULT NULL,
+  `no_of_passengers` int(5) DEFAULT NULL,
+  `lounge_access` varchar(5) DEFAULT NULL,
+  `priority_checkin` varchar(5) DEFAULT NULL,
+  `insurance` varchar(5) DEFAULT NULL,
+  `payment_id` varchar(20) DEFAULT NULL,
+  `customer_id` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `train_details`
+--
+
+CREATE TABLE `train_details` (
+  `train_id` varchar(10) NOT NULL,
+  `train_type` varchar(20) DEFAULT NULL,
+  `total_capacity` int(5) DEFAULT NULL,
+  `active` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Дамп данных таблицы `train_details`
+--
+
+INSERT INTO `train_details` (`train_id`, `train_type`, `total_capacity`, `active`) VALUES
+('10001', 'Train1', 300, 'Yes'),
+('10002', 'Train2', 275, 'Yes'),
+('10003', 'Train3', 50, 'Yes'),
+('10004', 'Train4', 225, 'Yes'),
+('10007', 'Train5', 250, 'Yes');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Индексы таблицы `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Индексы таблицы `frequent_psng_details`
+--
+ALTER TABLE `frequent_psng_details`
+  ADD PRIMARY KEY (`frequent_psng_no`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Индексы таблицы `passengers`
+--
+ALTER TABLE `passengers`
+  ADD PRIMARY KEY (`passenger_id`,`pnr`),
+  ADD KEY `pnr` (`pnr`),
+  ADD KEY `frequent_flier_no` (`frequent_psng_no`);
+
+--
+-- Индексы таблицы `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `pnr` (`pnr`);
+
+--
+-- Индексы таблицы `railway_details`
+--
+ALTER TABLE `railway_details`
+  ADD PRIMARY KEY (`railway_no`,`departure_date`),
+  ADD KEY `jet_id` (`train_id`);
+
+--
+-- Индексы таблицы `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD PRIMARY KEY (`pnr`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `journey_date` (`journey_date`),
+  ADD KEY `flight_no` (`railway_no`),
+  ADD KEY `flight_no_2` (`railway_no`,`journey_date`);
+
+--
+-- Индексы таблицы `train_details`
+--
+ALTER TABLE `train_details`
+  ADD PRIMARY KEY (`train_id`);
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `frequent_psng_details`
+--
+ALTER TABLE `frequent_psng_details`
+  ADD CONSTRAINT `frequent_psng_details_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `passengers`
+--
+ALTER TABLE `passengers`
+  ADD CONSTRAINT `passengers_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `ticket_details` (`pnr`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `passengers_ibfk_2` FOREIGN KEY (`frequent_psng_no`) REFERENCES `frequent_psng_details` (`frequent_psng_no`) ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD CONSTRAINT `payment_details_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `ticket_details` (`pnr`) ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `railway_details`
+--
+ALTER TABLE `railway_details`
+  ADD CONSTRAINT `railway_details_ibfk_1` FOREIGN KEY (`train_id`) REFERENCES `train_details` (`train_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD CONSTRAINT `ticket_details_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ticket_details_ibfk_3` FOREIGN KEY (`railway_no`,`journey_date`) REFERENCES `railway_details` (`railway_no`, `departure_date`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
